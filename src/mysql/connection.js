03c26dee083efc623eec623894ac4f15205f63f8
JDev-Code <javier.diego.dev@gmail.com>
2022-11-29 13:36:26 +0100

    mysql2

--- src/mysql/connection.js
+++ src/mysql/connection.js
@@ -1,20 +1,29 @@
-import mysql from 'mysql'
+import mysql from 'mysql2/promise'
 
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
+  host: "containers-us-west-65.railway.app",
+  user: "root",
+  password: "aaa111!!!",
+  database: "railway",
+  port: 7312
+})
+
+export default conn
+
+
+
+/* const conn = mysql.createConnection({
   host: "we-social-app-database.cqfsypqe5mgn.eu-west-3.rds.amazonaws.com",
   user: "masterUsername",
   password: "aaa111!!!",
   database: "we-db"
-})
+})  */
 
-conn.connect(function (err) {
+/* conn.connect(function (err) {
   try {
     if (err) throw err
     console.log("DATABASE CONNECTED!")
   } catch {
     console.log('ERROR: ' + err.code)
   }
-})
-
-
-export default conn
+}) */
